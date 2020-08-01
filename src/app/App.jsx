63984f26022cf88d29d71ca17862b447dd9ca5d3
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-02 01:13:20 +0300

    init react app

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <span>Hello world !</span>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import * as serviceWorker from "./utils/serviceWorker";
+import "./index.css";
+
+import App from "./app/App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+
+    document.getElementById("root")
+);
+
+serviceWorker.unregister();
