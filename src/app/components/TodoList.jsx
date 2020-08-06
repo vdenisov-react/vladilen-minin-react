cdebd883948cbdbfd93c6f34d353e02563cd141b
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-06 13:44:31 +0300

    03. alternative way to set styles

--- src/app/components/TodoList.jsx
+++ src/app/components/TodoList.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0,
+    },
+};
+
 export default function TodoList() {
     return (
-        <ul>
+        <ul style={styles.ul}>
             <li>1</li>
             <li>2</li>
         </ul>
