5569cb202c1962d03f7d07528817a844e72a3ac2
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-06 13:01:21 +0300

    02. create simple list component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
     return (
         <div className="wrapper">
             <h1>React tutorial</h1>
+
+            <TodoList />
         </div>
     );
 }
