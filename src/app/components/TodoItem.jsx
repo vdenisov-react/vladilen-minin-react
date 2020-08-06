2a2b4d6be5e611007831a94f867c28e8344a9677
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-06 15:25:11 +0300

    08. improve visibility using styles

--- src/app/components/TodoItem.jsx
+++ src/app/components/TodoItem.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem',
+    },
+    element: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    input: {
+        marginRight: '1rem',
+    },
+};
+
 function TodoItem({ todo, index }) {
     return (
-        <span>
-            <strong>{index + 1}</strong>
-            &nbsp;
-            {todo.title}
-        </span>
+        <div style={styles.container}>
+            <span style={styles.element}>
+                <input type="checkbox" style={styles.input} />
+                <strong>{index + 1}</strong>
+                &nbsp;
+                {todo.title}
+            </span>
+
+            <button className="rm-btn">&times;</button>
+        </div>
     );
 }
 
